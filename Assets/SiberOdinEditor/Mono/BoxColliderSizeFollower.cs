f8ff886ca9815f8efbd9e8a2a09728ccb8b7bf0a
SiberDesign <siberdesign2020@gmail.com>
2023-11-17 10:40:48 +0800

    [新增] 🌱 增加一個調整 BoxCollider2D 能跟隨Sprite Bounds Size 的工具

---
+++ Assets/SiberOdinEditor/Mono/BoxColliderSizeFollower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SiberOdinEditor.Mono
+{
+    public class BoxColliderSizeFollower : MonoBehaviour
+    {
+        [SerializeField]
+        private List<BoxCollider2D> colliders;
+
+        [SerializeField]
+        private SpriteRenderer spriteRenderer;
+
+        [SerializeField]
+        private float scale = 1f;
+
+        private Vector2 changeBoundSize;
+        private Vector2 changeCenter;
+
+        [Button("跟隨 Sprite Size")]
+        private void FollowSize()
+        {
+            var boundsSize = (Vector2)spriteRenderer.sprite.bounds.size * scale;
+            var centerPos  = new Vector2(0, boundsSize.y / 2);
+            for (var i = 0; i < colliders.Count; i++)
+            {
+                colliders[i].size   = boundsSize;
+                colliders[i].offset = centerPos;
+
+                if (i != 0) continue; // 記錄一次
+                changeBoundSize = boundsSize;
+                changeCenter    = centerPos;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(changeCenter, changeBoundSize);
+        }
+    }
+}
